dc47e192b5756c6633123aed68d811b62dec9dbc
Joe Frangoudes <jfrangoudes@gmail.com>
2012-10-18 19:52:49 +0100

    new ImageBank class and client now renders stary background

--- src/Game.java
+++ src/Game.java
@@ -3,8 +3,11 @@ import java.awt.Graphics2D;
 
 public class Game {
 	
+	ImageBank imgBank;
+	
 	public Game(){
-		
+		imgBank = new ImageBank();
+		imgBank.loadImage("background", "background.jpg");
 	}
 	
 	/**
@@ -13,6 +16,6 @@ public class Game {
 	 * @return
 	 */
 	public void tick(Graphics2D g2d){
-		
+		g2d.drawImage(imgBank.getImage("background"), 0, 0, null);
 	}
 }
--- src/ImageBank.java
+++ src/ImageBank.java
@@ -1,4 +1,30 @@
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.imageio.ImageIO;
+
 
 public class ImageBank {
+	private HashMap<String ,Image> images;
 	
+	public ImageBank(){
+		images = new HashMap<String, Image>();
+	}
+	
+	public void loadImage(String key, String filename){
+		try {
+			Image img = ImageIO.read(new File(filename));
+			images.put(key, img);
+		} catch (IOException e) {
+			System.out.println("could not load image file " + filename);
+			e.printStackTrace();
+		}
+	}
+	
+	public Image getImage(String key){
+		return images.get(key);
+	}
 }
